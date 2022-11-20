4e33564d5cbac8b8e9951b1ce7224f57189fdce5
Thomas Nakagawa <thomasnakagawa@gmail.com>
2022-11-19 18:34:10 -0800

    Add project files.

---
+++ beepbeep/Program.cs
@@ -0,0 +1,23 @@
+﻿beep();
+foreach (string arg in args)
+{
+    switch(arg)
+    {
+        case "beep":
+            beep();
+            break;
+        case "boop":
+            boop();
+            break;
+    }
+}
+
+static void beep()
+{
+    Console.Beep(1000, 700);
+}
+
+static void boop()
+{
+    Console.Beep(800, 700);
+}
